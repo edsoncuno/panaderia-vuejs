d1dac0901a674832a3a1507d87ff361944392b02
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-01-11 09:07:04 -0500

    portada con sas falta media query

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
     <section class="principal">
-        <section class="portada" id="portada">
+        <section class="portada">
             <div class="columna">
                 <div class="foto izquierda lightbox">
                 </div>
@@ -35,93 +35,73 @@
         </section>
     </section>
 </template>
-<style scoped>
-.titulo h2 {
-    font-size: 3.125rem;
-    font-weight: 400;
-    line-height: 3.6875rem;
-    margin-bottom: 1.125rem;
-    font-style: italic;
-    letter-spacing: -0.02em;
-}
-
-.titulo p {
-    font-size: 1.25rem;
-    color: #777;
-}
-
+<style scoped lang="scss">
 .principal {
     height: 80rem;
-}
 
-.principal .portada {
-    display: flex;
-    flex-wrap: wrap;
-    /*
-    /*su valor por defecto el nowrap que hace que los elementos dentro sean responsive y se molden,
-    , pero con wrap los elementos dentro cumple con sus estilos dados y si no alcanza espacio ,
-    se continua en troa fila o columna, dependiendo de la direccion del flex*/
-    justify-content: space-between;
-    /*esto no tiene efecto por que los elementos estan ocupando el 100%,
-    si los editamos se arregla*/
-}
+    .portada {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
 
-.principal .portada .columna {
-    width: 50%;
-    background: black;
-    color: white;
-    display: flex;
-    flex-wrap: wrap;
-}
+        .columna {
+            width: 50%;
+            background: black;
+            color: white;
+            display: flex;
+            flex-wrap: wrap;
 
-.principal .portada .columna .foto {
-    background-size: cover;
-    background-position: center;
-    width: 50%;
-}
+            .foto {
+                background-size: cover;
+                background-position: center;
+                width: 50%;
+            }
 
-.principal .portada .columna .izquierda {
-    background-image: url('https://i.postimg.cc/Gh1Lz8Mn/portada1.jpg');
-    background-position: left;
-}
+            .izquierda {
+                background-image: url('https://i.postimg.cc/Gh1Lz8Mn/portada1.jpg');
+                background-position: left;
+            }
 
-.principal .portada .columna .derecha {
-    background-image: url('https://i.postimg.cc/m2NbnLqz/portada2.jpg');
-    opacity: 0.8
-}
+            .derecha {
+                background-image: url('https://i.postimg.cc/m2NbnLqz/portada2.jpg');
+                opacity: 0.8
+            }
 
-.principal .portada .columna .texto {
-    width: 50%;
-    padding: 3.375rem;
-}
+            .texto {
+                width: 50%;
+                padding: 3.375rem;
 
-.principal .portada .columna .texto h2 {
-    font-size: 1.8125rem;
-    font-weight: 400;
-    line-height: 2.6875rem;
-    margin-bottom: 1.125rem;
-}
+                h2 {
+                    font-size: 1.8125rem;
+                    font-weight: 400;
+                    line-height: 2.6875rem;
+                    margin-bottom: 1.125rem;
+                }
 
-.principal .portada .columna .texto p {
-    font-size: 0.9375rem;
-    line-height: 1.75rem;
-}
+                p {
+                    font-size: 0.9375rem;
+                    line-height: 1.75rem;
+                }
+            }
 
-.principal .portada .columna .foto_full {
-    width: 100%;
-    min-height: 15.25rem;
-    background-size: cover;
-    background-position: center;
-}
+            .foto_full {
+                width: 100%;
+                min-height: 15.25rem;
+                background-size: cover;
+                background-position: center;
+            }
 
-.principal .portada .columna .foto_full.izquierda {
-    background-image: url('https://i.postimg.cc/HWMWQ49t/portada3.jpg');
-    opacity: 0.8;
-}
+            .foto_full.izquierda {
+                background-image: url('https://i.postimg.cc/HWMWQ49t/portada3.jpg');
+                opacity: 0.8;
+            }
 
-.principal .portada .columna .foto_full.derecha {
-    background-image: url('https://i.postimg.cc/W3kVwTg4/portada4.jpg');
-    opacity: 0.8;
+            .foto_full.derecha {
+                background-image: url('https://i.postimg.cc/W3kVwTg4/portada4.jpg');
+                opacity: 0.8;
+            }
+        }
+    }
 }
 
 @media all and (max-width: 1500px) {
